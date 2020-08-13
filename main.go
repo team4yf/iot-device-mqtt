e128e4b250460de33d6cd78125f71a55493b72fe
yfsoftcom <yfsoftcom@126.com>
2020-08-13 10:24:03 +0800

    todo upgrade fpm-server

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/team4yf/iot-device-mqtt/hooks"
 
 	"github.com/team4yf/yf-fpm-server-go/fpm"
@@ -14,9 +12,9 @@ func main() {
 
 	app := fpm.New()
 
-	app.AddHook("BEFORE_INIT", func(f *fpm.Fpm) {
-		fmt.Println("run some hook")
-	}, 10)
+	// app.AddHook("BEFORE_INIT", func(f *fpm.Fpm) {
+	// 	fmt.Println("run some hook")
+	// }, 10)
 
 	app.AddHook("AFTER_INIT", hooks.ConsumerHook, 10)
 	app.Init()
